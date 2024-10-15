515bfa5d09c1154e4e5498837597064c6f01d7d9
Sharang Goel <goelshaz001@gmail.com>
2024-10-14 22:36:08 -0400

    added timer

--- app/timer.tsx
+++ app/timer.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet, ImageBackground } from 'react-native';
 import { CountdownCircleTimer, useCountdown } from 'react-native-countdown-circle-timer';
 import {BlurView} from '@react-native-community/blur';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 
 const colors = [
     '#37C536',
@@ -13,6 +15,7 @@ const colors = [
 
 const image = require('@/assets/images/wallpaper.jpg');
 const middle = require('@/assets/images/misc/spike-timer.jpg');
+const middle2 = require('@/assets/images/dark-blue-bg.jpg');
 
 const Timer: React.FC = () => {
 
@@ -25,8 +28,8 @@ const Timer: React.FC = () => {
 
             <BlurView
                 style={styles.absolute}
-                blurType="light"
-                blurAmount={5}
+                blurType="dark"
+                blurAmount={1}
                 reducedTransparencyFallbackColor="white"
             />
 
@@ -43,19 +46,29 @@ const Timer: React.FC = () => {
                 
                 {({remainingTime}) => {
                     return (
-                        <View style={{backgroundColor:"black", borderRadius: 250, width: 250, height: 250, overflow: 'hidden'}}>
-                            <ImageBackground source={image} style={{width: '100%', height: '100%', borderRadius: 200}}>
+                        <View style={styles.timerContainer}>
+                            {/* <ImageBackground source={middle2} style={{width: '100%', height: '100%', borderRadius: 200}}> */}
                             <Text style={styles.time}>{remainingTime}</Text>
-                            </ImageBackground>
+                            {/* </ImageBackground> */}
                         </View>
                     )
                 }}
                 </CountdownCircleTimer>
+
+                <View style={{display: 'flex', flexDirection: 'row', marginTop: 15, gap: 25}}>
+                    {/* <MaterialCommunityIcons name="restart" size={75} color="white" style={styles.shadow}/> */}
+                    {isPlaying ?
+                        <MaterialCommunityIcons onPress={()=>setIsPlaying(false)} name="pause" size={80} color="white" style={styles.shadow}/> :
+                        <MaterialCommunityIcons onPress={()=>setIsPlaying(true)} name="play" size={80} color="white" style={styles.shadow}/>
+                    }
+                    <MaterialCommunityIcons onPress={()=>setKey(prev => prev + 1)} name="restart" size={65} color="white" style={[styles.shadow, {marginTop: 10}]}/>
+                </View>
             </View>
 
-            <Button title='Pause' onPress={() => { setIsPlaying(false) }}></Button>
+            
+            {/* <Button title='Pause' onPress={() => { setIsPlaying(false) }}></Button>
             <Button title='Start' onPress={() => { setIsPlaying(true) }}></Button>
-            <Button title='Reset' onPress={() => { setKey(prev => prev + 1)}}></Button>
+            <Button title='Reset' onPress={() => { setKey(prev => prev + 1)}}></Button> */}
         </ImageBackground>
     );
 };
@@ -67,6 +80,20 @@ const styles = StyleSheet.create({
         marginBottom: 'auto', marginTop: 220
     },
 
+    timerContainer: {
+        backgroundColor: "#1b3148", 
+        borderRadius: 250, 
+        width: 250,
+        height: 250, 
+        overflow: 'hidden'
+    },
+
+    shadow: {
+        textShadowColor: 'black',
+        textShadowOffset: {width: 3, height: 3},
+        textShadowRadius: 10,
+    },
+
     time: {
         marginTop:'auto',
         marginBottom: 'auto',
@@ -76,6 +103,7 @@ const styles = StyleSheet.create({
         textShadowColor: 'black',
         textShadowOffset: {width: 5, height: 5},
         textShadowRadius: 10,
+        fontFamily: 'Valorant',
     },
 
 
